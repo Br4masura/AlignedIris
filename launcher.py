bad30fa8ca7307a0154a9f3832960d413693c532
Br4masura <blackbird100804@gmail.com>
2025-07-08 22:44:20 -0600

    Fragrantly extinguished agony

---
+++ launcher.py
@@ -0,0 +1,8 @@
+# root/app.py
+import sys
+import os
+sys.path.append("aligned_iris_main")
+from aligned_iris_main import app_main  # Rename your real app entry function if needed
+
+if __name__ == "__main__":
+    app_main()
